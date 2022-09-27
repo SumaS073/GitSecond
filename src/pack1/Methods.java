4b5d7f028f2653edfa7f1d6f248ca41e2339522a
Suma S <sumaduttesh@gmail.com>
2022-09-27 16:46:12 +0530

    entire GitSecond project added

---
+++ src/pack1/Methods.java
@@ -0,0 +1,20 @@
+package pack1;
+
+class Demo
+{
+void m1()
+{
+System.out.println("Hi");	
+}
+
+}
+
+class Methods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Demo d1 = new Demo();
+		d1.m1();
+	}
+
+}
